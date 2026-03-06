97637bf2bded71abfa70214752de3bfe8bdeb6b5
Boris Alekseev <borisalekseev95@gmail.com>
2026-03-06 20:17:01 +0200

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,21 @@ export default {
   server: {
     port: 8080,
     hot: true
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "bootstrap/scss/bootstrap";`
+      }
+    }
   }
 }
