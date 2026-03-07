f801ab6f06df51e7fda26b6e589e62d993f0b66e
Boris Alekseev <borisalekseev95@gmail.com>
2026-03-07 19:27:15 +0200

    add localization with i18next

---
+++ src/js/i18n.js
@@ -0,0 +1,14 @@
+import i18next from 'i18next'
+import ru from '../locales/ru'
+
+const initI18n = () => {
+  return i18next.init({
+    lng: 'ru',
+    debug: false,
+    resources: {
+      ru,
+    },
+  })
+}
+
+export default initI18n
---
+++ src/locales/ru.js
@@ -0,0 +1,27 @@
+export default {
+  translation: {
+    form: {
+      success: 'RSS успешно загружен',
+      label: 'Ссылка RSS',
+      submit: 'Добавить',
+    },
+    feeds: {
+      title: 'Фиды',
+    },
+    posts: {
+      title: 'Посты',
+      preview: 'Просмотр',
+    },
+    modal: {
+      fullArticle: 'Читать полностью',
+      close: 'Закрыть',
+    },
+    errors: {
+      required: 'Не должно быть пустым',
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      duplicate: 'RSS уже существует',
+      networkError: 'Ошибка сети',
+      parseError: 'Ресурс не содержит валидный RSS',
+    },
+  },
+}
