fa724bda4c5a4c1ae83a3bdb9d338bd8dfc628b0
Boris Alekseev <borisalekseev95@gmail.com>
2026-03-07 20:11:44 +0200

    edit path source

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default {
-  root: path.resolve(__dirname, 'src'),
+  root: path.resolve(__dirname, ''),
   server: {
     port: 8080,
     hot: true
